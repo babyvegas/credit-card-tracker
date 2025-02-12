a8000e89381cd1789d3edf5ad05a4ced0aa58d6f
= <=>
2025-02-11 20:41:40 -0600

    first commit and read me

---
+++ src/main/java/com/donovanbarrientos/cardtracker/card_tracker/CardTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.donovanbarrientos.cardtracker.card_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donovanbarrientos/cardtracker/card_tracker/CardTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donovanbarrientos.cardtracker.card_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
